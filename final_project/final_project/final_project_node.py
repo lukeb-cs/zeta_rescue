6b23d1a334664427ab2e16e1bc5842d9a2f62bec
Luke B <luke.brenn04@gmail.com>
2025-12-01 19:09:21 -0500

    Wrote starting code for aruco action

--- final_project/final_project/final_project_node.py
+++ final_project/final_project/final_project_node.py
@@ -7,6 +7,7 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import random
+import math
 
 # Message types
 from sensor_msgs.msg import Image
@@ -15,6 +16,11 @@ from geometry_msgs.msg import TwistStamped
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
+class Person:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 class ZetaNode(rclpy.node.Node):
     def __init__(self):
         super().__init__('zeta')
@@ -22,12 +28,24 @@ class ZetaNode(rclpy.node.Node):
 
         self.subscription_img = self.create_subscription(Image, '/oakd/rgb/preview/image_raw', self.image_interp_callback, 10)
         self.subscription_aruco = self.create_subscription(Image, '/aruco_poses', self.aruco_pose_callback, 10)
+        self.subscription_pos = self.create_subscription(Odometry, "/odom", self.pos_callback, 10)
 
         self.thrust_pub = self.create_publisher(TwistStamped, 'cmd_vel', 10)
 
         self.timer = self.create_timer(1.0, self.change_motion_callback)
 
-    def image_interp_callback(self, msg):
+        self.scanning_code = False
+
+        self.x = 0.0
+        self.y = 0.0
+
+        self.person_list = []
+
+    def pos_callback(self, msg):
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+
+    def image_interp_callback(self, msg): # may not be used in final version
         img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         pumpkin_location = self.image_helper(img)
         if pumpkin_location[0] == -1 and pumpkin_location[1] == -1:
@@ -36,25 +54,6 @@ class ZetaNode(rclpy.node.Node):
             self.get_logger().info("Orange object located in front of robot")
             if pumpkin_location[1] < 100: # checking if the orange pixel is located at the top of the image
                 self.get_logger().info("Orange object is close to the robot")
-
-    def aruco_pose_callback(self, poses):
-        self.get_logger().info("Aruco is running the callback")
-        self.get_logger().info(f"Poses: {poses}")
-
-        # using the distance and agle, with trig functions, get the x and y position from the robots frame
-        # take the pose you were given and change it to be in the world frame
-        # normalize the world frame coords to remove small variance in points
-        # make a data structure, probably a list of objects to store all people locations in world frame coords
-        # decide if you can move, so that the robot is faceing the person
-            # either move along the x axi-s until difference in x is zero (check if possible)
-            # or you can move along the y-axis until the y is the same as the person
-        # turn towards the person once the x or y is the same
-        # make sure you can still see the aruco code
-        # move within a meter of the person and take a picture where the name is visible
-            # (optional) you could using orange detection to prevent the bot from getting too close, but that may not be needed
-            # if we are on the side of the robot where there is no code, move around the person until the code is found
-            # remember that is the aruco is found, its on one of the sides facing the robot, so we only need to check those sides
-        # retrace steps, so that you end back on the random path
 
     def image_helper(self, img):
         r = img[:, :, 2].astype(np.float32)
@@ -68,7 +67,53 @@ class ZetaNode(rclpy.node.Node):
             return (-1, -1)         # lesser oranges have a lower number, around maxVal = 100
         return maxLoc
 
+    def aruco_pose_callback(self, poses):
+        if self.scanning_code:
+            return
+        self.get_logger().info("Aruco is running the callback")
+        self.get_logger().info(f"Poses: {poses}")
+
+        angle = 0 # placeholder
+        distance = 1 # placeholder
+        scanning_code = True
+
+        # using the distance and angle, with trig functions, get the x and y position
+        x_dist = length * math.cos(math.radians(angle_deg))
+        y_dist = length * math.sin(math.radians(angle_deg))
+        x_loc = x_dist + self.x
+        y_loc = y_dist + self.y
+        
+        # normalize the coords to remove small variance in points
+            # if the value is close to that of something we've already scanned, set scanning_code = False and return
+        person = Person(x_loc, y_loc)
+        for p in self.person_list:
+            if abs(p.x - person.x) < 0.5 or abs(p.y - person.y) < 0.5:
+                self.scanning_code = False
+                return
+        self.person_list.append(person)
+
+        # store the robot's starting location
+        stating_x = self.x
+        starting_y = self.y
+
+        # make sure the robot rotates to travel parallel to the person
+
+        # decide if you can move, so that the robot is faceing the person
+            # either move along the x axis until difference in x is zero (check if possible)
+            # or you can move along the y-axis until the y is the same as the person
+
+        # turn towards the person once the x or y is the same
+        # move within a meter of the person and take a picture where the name is visible
+            # (optional) you could using orange detection to prevent the bot from getting too close, but that may not be needed
+            # if we are on the side of the robot where there is no code, move around the person until the code is found
+            # remember that is the aruco is found, its on one of the sides facing the robot, so we only need to check those sides
+        # retrace steps, so that you end back on the random path
+        
+        self.scanning_code = False
+
     def change_motion_callback(self):
+        if self.scanning_code:
+            return
         dumb_twister = Twist()
         linear_vec = Vector3()
         linear_vec.x = 0.2
