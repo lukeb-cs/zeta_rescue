dcb86a63d3b1338053c807d51d5124750bd5fa16
jmdebes <debesjm@dukes.jmu.edu>
2025-12-10 18:02:52 -0500

    aruco runs

--- launch/rescue.launch.py
+++ launch/rescue.launch.py
@@ -1,13 +1,27 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
+
+    aruco_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('ros2_aruco'),
+                
+                'launch',
+                'aruco_recognition.launch.py'  # change if your launch file has a different name
+             )
+         )
+        
+    )
+
     return LaunchDescription([
-        Node(
-            package='ros2_aruco',
-            executable='aruco_node',
-            name='aruco_node'
-        ),
+        aruco_launch,
         Node(
             package='zeta_rescue',
             executable='zeta_node',
